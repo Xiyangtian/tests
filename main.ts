c032cbffc4300f342397b0098cdac88758ea3c4a
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-06 15:15:06 +0800

    更新 pxt.json, main.blocks, main.ts, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,3 @@ scene_util.captureRegisteringRoomScenes(() => {
     wonui_cookhouse.prepare_cookhouse()
     game.popScene()
 })
-scene_util.captureRegisteringRoomScenes(() => {
-    mushroomfarm.prepare_mushroomfarm()
-    game.popScene()
-})
-
-cbland.startVillage()
