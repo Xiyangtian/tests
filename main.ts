29a9175f3cdcf054569d03ba682f370cdd141689
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-06 15:44:09 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+game.pushScene()
+
 scene_util.captureRegisteringRoomScenes(() => {
     wonui_cookhouse.prepare_cookhouse()
     game.popScene()
 })
 
-cbland.startVillage()
+cbland.startVillage(false)
