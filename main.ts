b4992642bde0a614941f87af870dfd2cd7fa5caf
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-06 14:13:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,15 +6,5 @@ scene_util.captureRegisteringRoomScenes(() => {
     mushroomfarm.prepare_mushroomfarm()
     game.popScene()
 })
-cbland_equipment.registerEquipment("空手", img`
-1 1 1 1 1 1 1 1
-1 . 5 . 5 . . 1
-1 5 5 5 5 . 5 1
-1 5 5 5 . 5 . 1
-1 5 5 5 5 . . 1
-1 . 5 5 5 5 . 1
-1 . . 5 5 . 5 1
-1 1 1 1 1 1 1 1
-`, 0)
 
 cbland.startVillage()
