b16d138ea08d7f5e5694c77c6df3bfebb827fd89
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-08 23:19:02 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,14 @@ game.pushScene()
     game.popScene()
 })*/
 
+scene_util.captureRegisteringRoomScenes(() => {
+    mushroomfarm.prepare_mushroomfarm()
+    game.popScene()
+})
+
+scene_util.captureRegisteringRoomScenes(() => {
+    fishingpond.prepare_fishingpond()
+    game.popScene()
+})
+
 cbland.startVillage(false)
